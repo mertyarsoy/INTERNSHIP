b01c568f160061ff38295f0e1414e923eb225907
Vitaliy An <130189212+VitaliyAnn@users.noreply.github.com>
2023-05-29 14:06:44 -0400

    Added - Create a product in QA with admin privileges/ API Testing

--- src/test/java/api/runners/Runner.java
+++ src/test/java/api/runners/Runner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "api/stepdefinitions",
         dryRun = false,
-        tags = "@API_Mert",
+        tags = "@API_Vitaliy",
         snippets = CucumberOptions.SnippetType.CAMELCASE,
         plugin = {"pretty", "html:target/uiReport.html", "rerun:target/uiFailedTests.txt",
                 "json:target/cucumber-reports/cucumber.json"}
--- src/test/java/utils/PayloadUtils.java
+++ src/test/java/utils/PayloadUtils.java
@@ -15,4 +15,40 @@ public class PayloadUtils {
 
         return msgPayload;
     }
+    public static String getRecordPayload(String petName, String gender, String postCode){
+        String recordPayload = "{\n" +
+                "  \"id\": 777,\n" +
+                "  \"name\": \"Barney\",\n" +
+                "  \"gender\": \"male\",\n" +
+                "  \"birthDate\": \"2019-12-25\",\n" +
+                "  \"address\": \"1111 east 11st avenue\",\n" +
+                "  \"city\": \"Tampa\",\n" +
+                "  \"state\": \"FL\",\n" +
+                "  \"country\": \"USA\",\n" +
+                "  \"phone\": \"111-111-1111\",\n" +
+                "  \"email\": \"barney@gmail.com\",\n" +
+                "  \"postcode\": \"11111\",\n" +
+                "  \"height\": 1,\n" +
+                "  \"weight\": 1,\n" +
+                "  \"bmi\": 1\n" +
+                "}";
+        return recordPayload;
+    }
+    public static String getProductPayload(){
+        String productPayload = "{\n" +
+                "  \"id\": 69,\n" +
+                "  \"name\": \"Amlodipine\",\n" +
+                "  \"description\": \"drug\",\n" +
+                "  \"image\": \"string\",\n" +
+                "  \"price\": 14,\n" +
+                "  \"inStock\": true,\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 12,\n" +
+                "    \"name\": \"Vitaliy\",\n" +
+                "    \"active\": false\n" +
+                "  }\n" +
+                "}";
+        return productPayload;
+
+    }
 }
